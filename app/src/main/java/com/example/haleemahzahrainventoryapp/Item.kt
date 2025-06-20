7d1b30f4bac6c275c77d370154b0a6eeae8a56cb
zhaleemah9 <zhaleemah9@gmail.com>
2025-06-20 18:22:33 +0300

    Delete app directory

--- app/src/main/java/com/example/haleemahzahrainventoryapp/Item.kt
+++
@@ -1,9 +0,0 @@
-package com.example.haleemahzahrainventoryapp
-
-data class Item(
-    var id: String? = null,
-    var name: String = "",
-    var description: String = "",
-    var quantity: Int = 0,
-    var status: String = "In-Stock"
-)
